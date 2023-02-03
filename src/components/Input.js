5f39ed54af046903e4817b509c112b3d0a7cde87
TheHuntsman4 <anikethvij464@gmail.com>
2023-02-03 21:41:04 +0530

    enter key fucntionality

--- src/components/Input.js
+++ src/components/Input.js
@@ -7,18 +7,21 @@ export default function Input(){
 const [text,setText]=useState("");
 const [todo,setTodo]=useState([]);
 
+const handleKey=event=>{
+    if(event.keyCode==13){
+        listPush();
+    }
+};
+
 const listPush=()=>{
     
     todo.push(text);
     setTodo(todo);
     setText("");
     
-
-
 };
 
 const handleChange=(event)=>{
-
     setText(event.target.value);
 };
 
@@ -26,7 +29,7 @@ const handleChange=(event)=>{
 return (
     <>
     <center>
-        <input type="text" id="message" name="todolist_element" onChange={handleChange} value={text} placeholder="Add your tasks here!!"/>
+        <input type="text" id="message" name="todolist_element" onChange={handleChange} value={text} placeholder="Add your tasks here!!" onKeyDown={handleKey}/>
         <button onClick={listPush}>Updated</button>
         <br /><br />
         </center>
