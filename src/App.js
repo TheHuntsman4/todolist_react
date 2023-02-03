47bf593a9dff51ceaf28e2df2055a2a66c3e1c92
TheHuntsman4 <anikethvij464@gmail.com>
2023-02-03 18:32:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+import Header from "./Header";
+import Input from "./Input";
+
+export default function App(){
+
+  return (
+      <>
+        <Header />
+        <Input />
+      </>
+    );
+}
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Header(){
+    return (
+        <>
+        <h2 align ="center">Welcome to the TodoList site</h2></>
+    );
+}
